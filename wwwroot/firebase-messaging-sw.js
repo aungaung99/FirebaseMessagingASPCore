e041c574f8e0de4232a016a187c1df3e3a8fb448
Aung Naing Oo <96081327+efficient82@users.noreply.github.com>
2022-03-02 17:26:52 +0630

    Add project files.

---
+++ wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+﻿console.log('ON backgrouond');
+
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+firebase.initializeApp({
+    apiKey: "AIzaSyDQs51qqcp_v2nd-FizNad3jA_cDCgt0T8",
+    authDomain: "testfirebasenoti-a8fff.firebaseapp.com",
+    projectId: "testfirebasenoti-a8fff",
+    storageBucket: "testfirebasenoti-a8fff.appspot.com",
+    messagingSenderId: "103338253557",
+    appId: "1:103338253557:web:7efcb9662ca6be08f3b9bc",
+    measurementId: "G-1WD96HTRK1"
+});
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    //const notificationTitle = 'Background Message Title';
+    //const notificationOptions = {
+    //    body: 'Background Message body.',
+    //    icon: '/firebase-logo.png'
+    //};
+
+    //self.registration.showNotification(notificationTitle,
+    //    notificationOptions);
+});
---
+++ wwwroot/firebase.js
@@ -0,0 +1,50 @@
+﻿firebase.initializeApp({
+    apiKey: "AIzaSyDQs51qqcp_v2nd-FizNad3jA_cDCgt0T8",
+    authDomain: "testfirebasenoti-a8fff.firebaseapp.com",
+    projectId: "testfirebasenoti-a8fff",
+    storageBucket: "testfirebasenoti-a8fff.appspot.com",
+    messagingSenderId: "103338253557",
+    appId: "1:103338253557:web:7efcb9662ca6be08f3b9bc",
+    measurementId: "G-1WD96HTRK1"
+});
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+Notification.requestPermission().then(function (permission) {
+	if (permission == "granted") {
+        console.log('Notification Permission Granted');
+        getDeviceToken();
+	}
+	else if (permission == "denied") {
+		console.log('Notification Permission Denied');
+	}
+}).catch(function (err) { });
+
+function getDeviceToken() {
+    // Get registration token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    messaging.getToken({ vapidKey: 'BH35UnsNCVCTpQhvjAAMYUfahPKRpwC0LyQwbAPAIqsk1oykxgS0hikxrME7vqp5Q5XwhHKxxHXU3ixnUwdhZgc' }).then((currentToken) => {
+        if (currentToken) {
+            // Send the token to your server and update the UI if necessary
+            console.log(currentToken);
+        } else {
+            // Show permission request UI
+            console.log('No registration token available. Request permission to generate one.');
+            // ...
+        }
+    }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+        // ...
+    });
+}
+
+messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    const title = JSON.parse(JSON.stringify(payload)).notification.title;
+    const body = JSON.parse(JSON.stringify(payload)).notification.body;
+    document.getElementById('exampleModalLabel').innerHTML = title;
+    document.getElementById('exampleModalText').innerHTML = body;
+    $('#exampleModal').modal('show');
+    // ...
+});
